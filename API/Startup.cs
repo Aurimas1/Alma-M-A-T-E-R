f15dc08b0f9c2db4b81e0b71c3a8e69eda6bb359
Aurimas.Laureckis <aurimas.laureckis@bentley.com>
2019-03-07 22:33:58 +0200

    this was no needed

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,11 +25,6 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "wwwroot";
-            });
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
