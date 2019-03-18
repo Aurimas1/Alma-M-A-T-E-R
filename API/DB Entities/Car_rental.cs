f5dbbee393fa8928bcb9cb97a8cd990bd8ab9bb8
Steror <tomas.kizielas@gmail.com>
2019-03-18 18:09:26 +0200

    Created classes for DB Entities
    Created 7 entities, might need another one for saving each trip's check list.

---
+++ API/DB Entities/Car_rental.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class Car_rental
+    {
+        public int Car_rentalID { get; set; }
+        public string Car_rental_url { get; set; }
+        public string Car_rental_company { get; set; }
+        public DateTime Car_issue_date { get; set; }
+        public DateTime Car_return_date { get; set; }
+        public string Price { get; set; }
+
+        public int TripID { get; set; }
+        public virtual Trip Trip { get; set; }
+    }
+}
---
+++ API/DB Entities/Plane_ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class Plane_ticket
+    {
+        public int Plane_ticketID { get; set; }
+        public string Plane_ticket_url { get; set; }
+        public string Plane_ticket_company { get; set; }
+        public string Airport { get; set; }
+        public DateTime Arrival_date { get; set; }
+        public DateTime Departure_date { get; set; }
+        public string Price { get; set; }
+
+        public int EmployeeID { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        public int TripID { get; set; }
+        public virtual Trip Trip { get; set; }
+    }
+}
