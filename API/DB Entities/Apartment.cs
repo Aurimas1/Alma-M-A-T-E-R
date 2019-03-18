f60d944cebf0c57c068e5e46b129f650ec9b8bf3
Steror <tomas.kizielas@gmail.com>
2019-03-18 18:09:26 +0200

    Created classes for DB Entities
    Created 7 entities, might need another one for saving each trip's check list.

---
+++ API/DB Entities/Apartment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class Apartment
+    {
+        public int ApartmentID { get; set; }
+        public string Company { get; set; }    //DevB or 3rd party
+        public string Address { get; set; }
+        public string Room_number { get; set; }
+        public DateTime Arrival_date { get; set; }
+        public DateTime Departure_date { get; set; }
+        public int Price { get; set; }
+
+        public int EmployeeID { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        public int TripID { get; set; }
+        public virtual Trip Trip { get; set; }
+    }
+}
