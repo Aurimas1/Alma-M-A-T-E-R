3e721e03196d26162593fea74ec7d62b1bf38b2b
Steror <tomas.kizielas@gmail.com>
2019-03-21 18:27:27 +0200

    Added some values

--- API/DB Entities/Apartment.cs
+++ API/DB Entities/Apartment.cs
@@ -8,12 +8,13 @@ namespace API
     public class Apartment
     {
         public int ApartmentID { get; set; }
-        public string Company { get; set; }    //DevB or 3rd party
+        public string Hotel_name { get; set; }    //DevB or 3rd party
         public string Address { get; set; }
         public string Room_number { get; set; }
-        public DateTime Arrival_date { get; set; }
-        public DateTime Departure_date { get; set; }
+        public DateTime Check_in { get; set; }
+        public DateTime Check_out { get; set; }
         public int Price { get; set; }
+        public string Apartment_reservation_url { get; set; }
 
         public int EmployeeID { get; set; }
         public virtual Employee Employee { get; set; }
--- API/DB Entities/Plane_ticket.cs
+++ API/DB Entities/Plane_ticket.cs
@@ -8,12 +8,12 @@ namespace API
     public class Plane_ticket
     {
         public int Plane_ticketID { get; set; }
-        public string Plane_ticket_url { get; set; }
-        public string Plane_ticket_company { get; set; }
+        public string Flight_company { get; set; }
         public string Airport { get; set; }
-        public DateTime Arrival_date { get; set; }
-        public DateTime Departure_date { get; set; }
+        public DateTime Forward_flight_date { get; set; }
+        public DateTime Return_flight_date { get; set; }
         public string Price { get; set; }
+        public string Plane_ticket_url { get; set; }
 
         public int EmployeeID { get; set; }
         public virtual Employee Employee { get; set; }
