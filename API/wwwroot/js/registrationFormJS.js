1671b7f7c2509bec8072fc883f709f501e71f670
Steror <tomas.kizielas@gmail.com>
2019-05-04 18:52:04 +0300

    Load offices to registrationForm

--- API/wwwroot/js/registrationFormJS.js
+++ API/wwwroot/js/registrationFormJS.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     //load EventsCalendar
     $("div.events_calendar").load("../Calendar_DB.html");
     //load list of employees from DB
-    loadEmployees().then(function() {
+    loadOffices().then(loadEmployees().then(function() {
         var table = $('#sort').DataTable();
 
         $('#tBody').on('click', 'tr', function () {
@@ -11,7 +11,7 @@ $(document).ready(function () {
             $("#rowSelected").text(table.rows('.selected').data().length + ' row(s) selected');
             $('#EmployeeNotification').css({ 'visibility': 'hidden' });
         });
-    })
+    }))
 });
 
 function CheckEmployees() {
@@ -44,28 +44,50 @@ function saveTrip() {
 }
 
 function loadEmployees() {
-        return $.ajax({
-            type: "GET",
-            url: '/api/employee',
-            contentType: "application/json",
-            xhrFields: {
-                withCredentials: true
-            },
-            success: function (data) {
-                var id = 1;
-                $.each(data, function (key, entry) {
-                    var line = $('<tr>');
-                    var fullName = entry.name;
-                    var splitName = fullName.split(" ");
-                    line.append($('<td data-table-header="Nr" id="NrColumn">').text(id))
-                        .append($('</td><td data-table-header="Name">').text(splitName[0]))
-                        .append($('</td><td data-table-header="Surname">').text(splitName[1]))
-                        .append($('</td><td data-table-header="Email">').text(entry.email))
-                        .append($('</td></tr>'));
-                    $('#tBody').append(line);
-                    id++;
-                });
-            },
-            error: function () {alert('Internet error'); },
-        })
+    return $.ajax({
+        type: "GET",
+        url: '/api/employee',
+        contentType: "application/json",
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function (data) {
+            var id = 1;
+            $.each(data, function (key, entry) {
+                var line = $('<tr>');
+                var fullName = entry.name;
+                var splitName = fullName.split(" ");
+                line.append($('<td data-table-header="Nr" id="NrColumn">').text(id))
+                    .append($('</td><td data-table-header="Name">').text(splitName[0]))
+                    .append($('</td><td data-table-header="Surname">').text(splitName[1]))
+                    .append($('</td><td data-table-header="Email">').text(entry.email))
+                    .append($('</td></tr>'));
+                $('#tBody').append(line);
+                id++;
+            });
+        },
+        error: function () {alert('Internet error'); },
+    })
+}
+
+function loadOffices() {
+    return $.ajax({
+        type: "GET",
+        url: '/api/office',
+        contentType: "application/json",
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function (data) {
+            $.each(data, function (key, entry) {
+                var line1 = $('<div class="form-check"><input class="form-check-input" type="radio" name="exampleRadios"onclick="'
+                + entry.city + '() " id="exampleRadios1" value="option1"><label class="form-check-label" for="exampleRadios1">'
+                + entry.city + ', ' + entry.country + '</label></div>');
+                var line2 = line1.clone();
+                $('#js-dep-office').append(line1);
+                $('#js-arr-office').append(line2);
+            });
+        },
+        error: function () {alert('Internet error'); },
+    })
 }
