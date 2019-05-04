811448186cc7b0a51b28462a02d7f69c9751385d
Steror <tomas.kizielas@gmail.com>
2019-05-04 13:47:36 +0300

    Clean up
    Selection still acting up

--- API/wwwroot/js/registrationFormJS.js
+++ API/wwwroot/js/registrationFormJS.js
@@ -1,25 +1,10 @@
 $(document).ready(function () {
 
-
     //load EventsCalendar
     $("div.events_calendar").load("../Calendar_DB.html");
     //load list of employees from DB
     loadEmployees();
-    /*$.ajax({
-        url: "",
-        type: "GET",
-        dataType: "json",
-        success: function (data) {
-            $.each(data, function () {
-                var line = $('<tr>');
-                line.append($('<th>').text($(this).Name))
-                    .append($('<th>').text($(this).Surname))
-                    .append($('<th>').text($(this).Email_address))
-            $('#tBody').append(line);
-            });
-        },
-        error: function () {alert('Internet error'); },
-    });*/
+
     var table = $('#sort').DataTable();
 
     $('#tBody').on('click', 'tr', function () {
@@ -64,22 +49,22 @@ function loadEmployees() {
             type: "GET",
             url: '/api/employee',
             contentType: "application/json",
-            /*xhrFields: {
+            xhrFields: {
                 withCredentials: true
-            },*/
+            },
             success: function (data) {
-                var i = 1;
+                var id = 1;
                 $.each(data, function (key, entry) {
                     var line = $('<tr>');
                     var fullName = entry.name;
                     var splitName = fullName.split(" ");
-                    line.append($('<td>').text(i))
-                        .append($('</td><td>').text(splitName[0]))
-                        .append($('</td><td>').text(splitName[1]))
-                        .append($('</td><td>').text(entry.email))
+                    line.append($('<td data-table-header="Nr" id="NrColumn">').text(id))
+                        .append($('</td><td data-table-header="Name">').text(splitName[0]))
+                        .append($('</td><td data-table-header="Surname">').text(splitName[1]))
+                        .append($('</td><td data-table-header="Email">').text(entry.email))
                         .append($('</td></tr>'));
                     $('#tBody').append(line);
-                    i++;
+                    id++;
                 });
             },
             error: function () {alert('Internet error'); },
