a840175b2ab4725dacd62c1231ca0c3cadb1a206
Steror <tomas.kizielas@gmail.com>
2019-05-05 20:11:27 +0300

    Added methods to load Employees attending trip and all Employees
    TripID set to constant(4) for now, change as needed.

--- API/wwwroot/js/FinalRegistrationFormJS.js
+++ API/wwwroot/js/FinalRegistrationFormJS.js
@@ -1,85 +1,86 @@
 $(document).ready(function () {
-    var table = $('#sort').DataTable();
-
+    
     //load EventsCalendar
     $("div.events_calendar").load("../Calendar_DB.html");
     
-    loadEmployees().then(function() {
-    $('tr:contains("Šmigelskis")').prop('disabled', true).css("cssText", "background-color:#D33F49 !important;");
-    $('tr:contains("Kiziela")').prop('disabled', true).css("cssText", "background-color:#D33F49 !important;");
+    loadTripEmployees().then(loadEmployees()).then(function() {
+        var table = $('#sort').DataTable();
 
-    $('#tBody').on('click', 'tr', function () {
-        $(this).toggleClass('selected');
-        $("#rowSelected").text(table.rows('.selected').data().length + ' row(s) selected');
-        $('#EmployeeNotification').css({ 'visibility': 'hidden' });
-    });
-    //----------------------------------------------------------
-    var employee = [['Tomas', 'Kiziela'], ['Marijus', 'Šmigelskis']];
-    var li = $('<li>').text("Tomas Kiziela").addClass("list-group-item").attr('id', 'li');
-    $('#employeeList').append(li);
-    var li = $('<li>').text("Marijus Šmigelskis").addClass("list-group-item").attr('id', 'li');
-    $('#employeeList').append(li);
+        $('tr:contains("Šmigelskis")').prop('disabled', true).css("cssText", "background-color:#D33F49 !important;");
+        $('tr:contains("Kiziela")').prop('disabled', true).css("cssText", "background-color:#D33F49 !important;");
 
-    var a = [['1', 'Jonas', 'Jonaitis'], ['2', '', ''], ['3', 'Povilas', 'Povilaitis'], ['4', '', ''], ['5', '', ''], ['6', 'Petras', 'Petraitis']];
-    //--------------------------------------------------------------
-    var i = 0;
-    $.each(a, function () {
-        var tr = $('<tr>');
-        var td = $('<td>').text($(this).get(0)).addClass('SmallColumn');
-        tr.append(td);
-        if ($(this).get(1) != '') {
-            tr.append('<td>The room is taken</td>');
-        }
-        else {
-            if (employee[i] != null) {
-                var name = employee[i][0] + " " + employee[i][1];
-                var dropDown = $('<select>');
-                $.each(employee, function () {
-                    var emp = $(this).get(0) + " " + $(this).get(1);
-                    dropDown.append("<option>" + emp + "</option>");
-                });
-                dropDown.prop('selectedIndex', i).css("width:100%");
-                td = $("<td>").append(dropDown);
-                tr.append(td);
-                $(`#li:contains(${name})`).text(name + '    - ' + $(this).get(0) + ' room');
-                alert(name);
-                i++;
+        $('#tBody').on('click', 'tr', function () {
+            $(this).toggleClass('selected');
+            $("#rowSelected").text(table.rows('.selected').data().length + ' row(s) selected');
+            $('#EmployeeNotification').css({ 'visibility': 'hidden' });
+        });
+        //----------------------------------------------------------
+        var employee = [['Tomas', 'Kiziela'], ['Marijus', 'Šmigelskis']];
+        var li = $('<li>').text("Tomas Kiziela").addClass("list-group-item").attr('id', 'li');
+        $('#employeeList').append(li);
+        var li = $('<li>').text("Marijus Šmigelskis").addClass("list-group-item").attr('id', 'li');
+        $('#employeeList').append(li);
+
+        var a = [['1', 'Jonas', 'Jonaitis'], ['2', '', ''], ['3', 'Povilas', 'Povilaitis'], ['4', '', ''], ['5', '', ''], ['6', 'Petras', 'Petraitis']];
+        //--------------------------------------------------------------
+        var i = 0;
+        $.each(a, function () {
+            var tr = $('<tr>');
+            var td = $('<td>').text($(this).get(0)).addClass('SmallColumn');
+            tr.append(td);
+            if ($(this).get(1) != '') {
+                tr.append('<td>The room is taken</td>');
             }
             else {
-                var dropDown = $('<select>');
-                $.each(employee, function () {
-                    var emp = $(this).get(0) + " " + $(this).get(1);
-                    dropDown.append("<option>" + emp + "</option>");
-                });
-                dropDown.append("<option></option>");
-                dropDown.prop('selectedIndex', i).css("width:100%;position:absolute");
-                td = $("<td>").append(dropDown);
-                tr.append(td);
-                i++;
+                if (employee[i] != null) {
+                    var name = employee[i][0] + " " + employee[i][1];
+                    var dropDown = $('<select>');
+                    $.each(employee, function () {
+                        var emp = $(this).get(0) + " " + $(this).get(1);
+                        dropDown.append("<option>" + emp + "</option>");
+                    });
+                    dropDown.prop('selectedIndex', i).css("width:100%");
+                    td = $("<td>").append(dropDown);
+                    tr.append(td);
+                    $(`#li:contains(${name})`).text(name + '    - ' + $(this).get(0) + ' room');
+                    alert(name);
+                    i++;
+                }
+                else {
+                    var dropDown = $('<select>');
+                    $.each(employee, function () {
+                        var emp = $(this).get(0) + " " + $(this).get(1);
+                        dropDown.append("<option>" + emp + "</option>");
+                    });
+                    dropDown.append("<option></option>");
+                    dropDown.prop('selectedIndex', i).css("width:100%;position:absolute");
+                    td = $("<td>").append(dropDown);
+                    tr.append(td);
+                    i++;
+                }
             }
-        }
-        $('#tableBody').append(tr);
-    });
-    if (employee[i] != null) {
-        var booking = 0;
-        $.each(employee, function () {
-            var name = employee[i] + " " + employee[i];
-            $(`li:contains(${name})`).text(name + '    - no room').css({ 'color': 'red' });
-            i++;
-            booking++;
+            $('#tableBody').append(tr);
         });
-        $('#listDiv').append(`<p>${booking} rooms need to be booking in the hotel</p>`);
-    }
+        if (employee[i] != null) {
+            var booking = 0;
+            $.each(employee, function () {
+                var name = employee[i] + " " + employee[i];
+                $(`li:contains(${name})`).text(name + '    - no room').css({ 'color': 'red' });
+                i++;
+                booking++;
+            });
+            $('#listDiv').append(`<p>${booking} rooms need to be booking in the hotel</p>`);
+        }
 
-    if ($("#CheckEmployeeCar").is(':checked')) {
-        $("#employee_car").show();
-    }
-    if ($("#CheckRentCar").is(':checked')) {
-        $("#car_rental").show();
-    }
-    if ($("#CheckPlane").is(':checked')) {
-        $("#plane_div").show();
-    }
+        if ($("#CheckEmployeeCar").is(':checked')) {
+            $("#employee_car").show();
+        }
+        if ($("#CheckRentCar").is(':checked')) {
+            $("#car_rental").show();
+        }
+        if ($("#CheckPlane").is(':checked')) {
+            $("#plane_div").show();
+        }
 
     })
 });
@@ -141,6 +142,26 @@ function openPlane(){
     else {
         $("#plane_div2").show();
     }
+}
+
+function loadTripEmployees() {
+    return $.ajax({
+        type: "GET",
+        url: '/api/trip/employees?id=4', //Insert current trip ID instead of 4
+        contentType: "application/json",
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function (data) {
+            $.each(data, function (key, entry) {
+                var fullName = entry.name;
+                var splitName = fullName.split(" ");
+                var line = $('<li class="list-group-item">' + splitName[0] + ' ' + splitName[1] + '</li>')
+                $('#SelectedEmployees').append(line);
+            });
+        },
+        error: function () {alert('Internet error'); },
+    })
 }
 
 function loadEmployees() {
