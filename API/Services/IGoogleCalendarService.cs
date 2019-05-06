c7ee9cb4a2a1d948221686449cd721d9b79cc8ef
Aurimas.Laureckis <aurimas.laureckis@bentley.com>
2019-05-06 08:49:36 +0300

    Save events for user

---
+++ API/Services/IGoogleCalendarService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public interface IGoogleCalendarService
+    {
+        Task<CalendarResponse> GetEvents(string accessToken);
+    }
+}
