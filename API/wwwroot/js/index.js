d875234d15de4c028b52bf5f6b317c8a48840a8c
Rūta Jankauskaitė <rutule9@gmail.com>
2019-05-11 20:53:44 +0300

    Change tabs visibility according to the user role

--- API/wwwroot/js/index.js
+++ API/wwwroot/js/index.js
@@ -1,4 +1,6 @@
-
+window.onload = function (){
+    getUser();
+};
 
 function tabChanged(elem){
     if (!$(elem).parent().hasClass("active")){
@@ -12,4 +14,32 @@ function tabChanged(elem){
 
 function goToTripCreation(elem){
     $("div#pageContent").load("../" + elem.id + ".html");
+}
+
+function getUser(){
+    $.ajax({
+        type: "GET",
+        url: "/api/employee/currentUser",
+        success: function(data, status){
+            setUsername(data.split(";")[0]);
+            hideTabs(data.split(";")[1]);
+        }});
+}
+
+function setUsername(username){
+    $("span.username").text(username);
+}
+
+function hideTabs(role){
+    console.log(role);
+    //during development
+    role = "Admin";
+    if (role !== "User"){
+        $("li.nav-item > a#statistics, li.nav-item > a#Trips").parent().css("display","unset");
+        $("li.nav-item.active").removeClass("active");
+        $("li.nav-item > a#Trips").addClass("active");
+        if (role === "Admin"){
+            $("li.nav-item > a#administration").parent().css("display","unset");
+        }
+    }
 }
