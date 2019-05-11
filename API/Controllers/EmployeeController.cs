40177e8c15c038d717b74a6694c6419668849cfd
Rūta Jankauskaitė <rutule9@gmail.com>
2019-05-11 20:53:44 +0300

    Change tabs visibility according to the user role

--- API/Controllers/EmployeeController.cs
+++ API/Controllers/EmployeeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Claims;
 using API.Services;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +26,14 @@ namespace API.Controllers
         public IEnumerable<Employee> Get()
         {
             return service.GetAll();
+        }
+        
+        // POST api/employee/currentUser
+        [HttpGet]
+        [Route("currentUser")]
+        public string GetCurrentUser()
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(User.Identity.GetUserName()) + ";" + (User.IsInRole("Admin")?"Admin":"") + (User.IsInRole("Organiser")?"Organiser":"User");
         }
     }
 }
