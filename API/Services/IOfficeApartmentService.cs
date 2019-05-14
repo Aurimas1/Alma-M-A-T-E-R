f7f978b2d0088432cc1e068c66182d6af9921079
Rūta Jankauskaitė <rutule9@gmail.com>
2019-05-14 15:40:10 +0300

    update/delete/create office apartments

--- API/Services/IOfficeApartmentService.cs
+++ API/Services/IOfficeApartmentService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace API.Services
 {
@@ -6,5 +7,8 @@ namespace API.Services
     {
         IEnumerable<Apartment> GetAll();
         IEnumerable<OfficeAndApartmentsDTO> GetAllOfficeApartments();
+        void UpdateApartment(Apartment apartment);
+        void DeleteApartment(int id);
+        Task<Apartment> CreateApartment(Apartment apartment);
     }
 }
--- API/Services/OfficeApartmentService.cs
+++ API/Services/OfficeApartmentService.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Constants;
 using API.Repositories;
 
@@ -28,12 +29,28 @@ namespace API.Services
             apartments.ForEach(e => officeAndApartmentsDtos.Add(new OfficeAndApartmentsDTO
             {
                 ApartmentId = e.ApartmentID,
-                Office = officeRepository.Get(e.OfficeId.GetValueOrDefault()).City + ", " + officeRepository.Get(e.OfficeId.GetValueOrDefault()).Country ,
+                Office = officeRepository.Get(e.OfficeId.GetValueOrDefault()).City + ", " + officeRepository.Get(e.OfficeId.GetValueOrDefault()).Country,
+                OfficeId = e.OfficeId.GetValueOrDefault(),
                 Name = e.Name,
                 Address = e.Address,
                 RoomNumber = e.RoomNumber
             }));
             return officeAndApartmentsDtos;
+        }
+
+        public void UpdateApartment(Apartment apartment)
+        {
+            apartmentsRepository.Update(apartment);
+        }
+
+        public void DeleteApartment(int id)
+        {
+            apartmentsRepository.Delete(id);
+        }
+        
+        public async Task<Apartment> CreateApartment(Apartment apartment)
+        {
+            return await apartmentsRepository.Add(apartment);
         }
     }
 
@@ -41,6 +58,7 @@ namespace API.Services
     {
         public int ApartmentId { get; set; }
         public string Office { get; set; }
+        public int OfficeId { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public int RoomNumber { get; set; }
