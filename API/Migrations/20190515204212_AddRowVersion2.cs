3f4356db1f78d82141de5be96194fefe3294c2d0
Elžbieta <>
2019-05-15 23:46:19 +0300

    add migration

---
+++ API/Migrations/20190515204212_AddRowVersion2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class AddRowVersion2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>("RowVersion", "Apartments", rowVersion: true, nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
