7301ddcec45fbb7431e7572c005b28aba6fec8db
Steror <tomas.kizielas@gmail.com>
2019-05-16 16:06:51 +0300

    Started trip merge wizard

---
+++ API/wwwroot/js/MergeWizard.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+
+    //load EventsCalendar
+    $("div#events_calendar1").load("../Calendar_DB.html");
+    $("div#events_calendar2").load("../Calendar_DB.html");
+});
