416950f364970cdf314a4f3d795df9550d0f1740
Aurimas.Laureckis <aurimas.laureckis@bentley.com>
2019-05-16 14:54:18 +0300

    Seeder

---
+++ Seeder/SeederDbContext.cs
@@ -0,0 +1,24 @@
+﻿using API;
+using Microsoft.EntityFrameworkCore;
+
+namespace Seeder
+{
+    public class SeederDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Apartment> Apartments { get; set; }
+        public DbSet<CarRental> CarRentals { get; set; }
+        public DbSet<EmployeeToTrip> EmployeeToTrips { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<GasCompensation> GasCompensations { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<PlaneTicket> PlaneTickets { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BigDb;Trusted_Connection=True;ConnectRetryCount=0");
+        }
+    }
+}
