3c233ab1d35b5eeb532c3889f0209258b3e72a26
Elžbieta <>
2019-05-19 22:47:16 +0300

    shows only approved employees

---
+++ API/Models/EmployeeToTripInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class EmployeeToTripInfo
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeEmail { get; set; }
+        public int EmployeeID { get; set; }
+        public string EmployeeStatus { get; set; }
+    }
+}
