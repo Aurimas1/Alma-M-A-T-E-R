f9469323da992c068000541747e506ca81164893
Steror <tomas.kizielas@gmail.com>
2019-05-20 11:28:44 +0300

    Revert "Changed bool to not NULLable"
    This reverts commit 6ce74093a09cd213e2f24f731260843fddcbdb60.

--- API/Migrations/20190519203919_AddIsApartmentNeeded.cs
+++ API/Migrations/20190519203919_AddIsApartmentNeeded.cs
@@ -7,10 +7,6 @@ namespace API.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.AddColumn<bool>("IsApartmentNeeded", "EmployeeToTrips", nullable: true);
-
-            migrationBuilder.Sql("Update [EmployeeToTrips] SET [IsApartmentNeeded]='true' WHERE [IsApartmentNeeded] IS null");
-
-            migrationBuilder.AlterColumn<bool>("IsApartmentNeeded", "IsApartmentNeeded", nullable: false);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
