5b1fd85b7ecea90d759789bc9c7c1ec733a69c27
Steror <tomas.kizielas@gmail.com>
2019-05-20 11:29:28 +0300

    Revert "Add migration"
    This reverts commit ccf310f72fbc32912dfacebe281a3219396692bb.

--- API/Migrations/ApiDbContextModelSnapshot.cs
+++ API/Migrations/ApiDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace API.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062")
+                .HasAnnotation("ProductVersion", "2.2.3-servicing-35854")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
@@ -99,8 +99,6 @@ namespace API.Migrations
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
                     b.Property<int>("EmployeeID");
-
-                    b.Property<bool>("IsApartmentNeeded");
 
                     b.Property<string>("Status");
 
