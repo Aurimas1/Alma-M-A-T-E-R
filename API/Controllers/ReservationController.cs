f19e12c4f510ae0dd794ab29677707afda8bf6e7
Steror <tomas.kizielas@gmail.com>
2019-05-24 20:24:32 +0300

    Added auth attributes to controllers and removed unnecessary usings

--- API/Controllers/ReservationController.cs
+++ API/Controllers/ReservationController.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using API.Repositories;
-using API.Services;
-using Microsoft.AspNet.Identity;
+﻿using API.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
